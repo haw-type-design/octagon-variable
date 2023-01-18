acde6d41dc93a6ac1877a6165d5a341978b66715
simonthi <github@simonthiefes.de>
2023-01-18 11:41:46 +0100

    Update prod pipeline, added dependencies

--- template/main/fontmake.sh
+++ template/main/fontmake.sh
@@ -1,7 +1,15 @@
+#       dependencies
+#           fontmake
+#           homebrew-webfonttools
+
 fontmake 221026_octagon.glyphs -i -o ttf --output-dir ../../export/static/ttf
 fontmake 221026_octagon.glyphs -i -o otf --output-dir ../../export/static/otf
-cp -R instance_ufo/OctagonVariable-HROT-30VROT-30.ufo master_ufo
-cp -R instance_ufo/OctagonVariable-HROT-30VROT30.ufo master_ufo
-cp -R instance_ufo/OctagonVariable-HROT30VROT-30.ufo master_ufo
-cp -R instance_ufo/OctagonVariable-HROT30VROT30.ufo master_ufo
-fontmake master_ufo/octagon_var.designspace -o variable --output-dir ../../export/variable/ttf
+cp -r instance_ufo/OctagonVariable-HROT-30VROT-30.ufo master_ufo
+cp -r instance_ufo/OctagonVariable-HROT-30VROT30.ufo master_ufo
+cp -r instance_ufo/OctagonVariable-HROT30VROT-30.ufo master_ufo
+cp -r instance_ufo/OctagonVariable-HROT30VROT30.ufo master_ufo
+fontmake master_ufo/octagon-variable.designspace -o variable --output-dir ../../export/variable/ttf
+mv ../../export/variable/ttf/octagon-variable-VF.ttf ../../export/variable/ttf/OctagonVariableVF.ttf 
+woff2_compress ../../export/variable/ttf/OctagonVariableVF.ttf
+mv ../../export/variable/ttf/OctagonVariableVF.woff2 ../../export/variable/woff2
+cp -r ../../export/variable/woff2/OctagonVariableVF.woff2 ../../web-specimen/fonts
