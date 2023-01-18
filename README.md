f00ae087d9461afae7ee11406c6b8ba840579f6f
simonthi <github@simonthiefes.de>
2023-01-18 09:42:56 +0100

    Initial push

---
+++ README.md
@@ -0,0 +1,15 @@
+# octagon-revival
+
+A repository to host and collect student contributions to the collaborative octagon variable revival font project hosted with the second semester students in the *Basisjahr Type Design LAB* course @HAW Hamburg in 2022 under the guidance of Prof. Pierre Pané-Farré and Simon Thiefes.
+The octagon variable revival project is based on a wood type by Edwin Allen, which was first shown in George Nesbitt’s 1838 “First Premium Wood Types Cut by Machinery”.
+See: Shields, David: The Rob Roy Kelly American Wood Type Collection: a history and catalog. Austin, University of Texas Press, 2022, p. 230-231.
+
+## Participating students
+Alexander Royter, Emma Schmalisch, Felix Willnauer, Friederike Temme, Greta Wachholz, Jason Tsiakas, Julia Baskal, Julia Schmidt, Kaja Wetzel, Kateryna Shelevytska, Lara Tamminga, Nina Vierke Liyani, Sania Salem, Sarah-Ann Röhlen, Sofiya Slyusarenko, Stina Scholz, Syeda Maya Zaheer, Vanessa Schneider, Zoe Rygus
+
+## References
+* Shields, David: The Rob Roy Kelly American Wood Type Collection: a history and catalog. Austin, University of Texas Press, 2022, p. 230-231.
+* Original printing blocks held in the Rob Roy Kelly Collection at *The Department of Art and Art History at The University of Texas at Austin*
+https://rrk.finearts.utexas.edu/?dt_portfolio=octagon
+* Andy Clymer’s Tilt Typeface <br>
+https://github.com/andyclymer/Tilt-Typeface
---
+++ web-specimen/js/main.js
@@ -0,0 +1,156 @@
+var accessGrant;
+
+window.onload = function() {
+    var width = window.screen.width;
+    var height = window.screen.height;
+    if (accessGrant == true)   {
+        $(".overlay").css( "display", "none");
+    }
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+        startup();
+    }
+    document.body.addEventListener("mousemove", (event) => {
+        if (event.x >= width/2){
+            if (event.y >= height/2){
+                $("#id1").css( "font-variation-settings", "'HROT'"+30/(width/2)*(event.x-width/2)+",'VROT'"+(-((30)/(height/2)*(event.y-height/2)))+"");
+                $("#id2").css( "font-variation-settings", "'HROT'"+30/(width/2)*(event.x-width/2)+",'VROT' 0");
+                $("#id11").css( "font-variation-settings", "'HROT' 0, 'VROT'"+(-((30)/(height/2)*(event.y-height/2)))+"");
+            } 
+
+            if (event.y < height/2) {
+                $("#id1").css( "font-variation-settings", "'HROT'"+30/(width/2)*(event.x-width/2)+",'VROT'"+(30 - (30/(height/2)*(event.y)))+"");
+                $("#id2").css( "font-variation-settings", "'HROT'"+30/(width/2)*(event.x-width/2)+",'VROT' 0");
+                $("#id11").css( "font-variation-settings", "'HROT' 0, 'VROT'"+(-((30)/(height/2)*(event.y-height/2)))+"");
+            }
+        }
+        else if (event.x < width/2){
+            if (event.y >= height/2){
+                $("#id1").css( "font-variation-settings", "'HROT'"+(- (30/(width/2)*(width/2 - event.x)))+",'VROT'"+(-((30)/(height/2)*(event.y-height/2)))+"");
+                $("#id2").css( "font-variation-settings", "'HROT'"+(- (30/(width/2)*(width/2 - event.x)))+",'VROT' 0");
+                $("#id11").css( "font-variation-settings", "'HROT' 0, 'VROT'"+(-((30)/(height/2)*(event.y-height/2)))+"");
+            } 
+
+            if (event.y < height/2) {
+                $("#id1").css( "font-variation-settings", "'HROT'"+(- (30/(width/2)*(width/2 - event.x)))+",'VROT'"+(30 - (30/(height/2)*(event.y)))+"");
+                $("#id2").css( "font-variation-settings", "'HROT'"+(- (30/(width/2)*(width/2 - event.x)))+",'VROT' 0");
+                $("#id11").css( "font-variation-settings", "'HROT' 0, 'VROT'"+(-((30)/(height/2)*(event.y-height/2)))+"");
+            }
+        } 
+    });
+}
+
+
+var colors = ['#ffcc7a', '#29ffb8', '#ff5a36', '#ffba00', '#cc7550', '#ff00f7', '#00b7eb', '#0040ff', '#ff9900', '#c79154', '#2fc475', '#32cd32', '#63e05c'];
+
+function randomize(values) {
+    let index = values.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (index != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * index);
+        index--;
+
+        // And swap it with the current element.
+        [values[index], values[randomIndex]] = [
+            values[randomIndex], values[index]];
+    }
+
+    return values;
+}
+
+
+
+function colorize() {
+    for(i = 0; i < 13; i++) {
+        $("#id"+i+"").css( "color", ""+colors[i]+"");
+        $("#id"+i+"").css( "border-color", ""+colors[i]+"");
+    }
+}
+
+function  run(){
+
+    $(".overlay").css( "display", "none");
+
+    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+        DeviceOrientationEvent.requestPermission()
+            .then(permissionState => {
+            if (permissionState === 'granted') {
+                window.addEventListener('deviceorientation', (e) => {
+                    const beta = e.beta;
+                    const gamma = e.gamma;
+                    if (beta < 45) {
+                        $("#id1").css( "font-variation-settings", "'HROT'"+(gamma/1.5)+",'VROT'"+((45-beta)/1)+"");
+                        $("#id2").css( "font-variation-settings", "'HROT'"+(gamma/1.5)+",'VROT' 0");
+                        $("#id11").css( "font-variation-settings", "'HROT' 0,'VROT'"+((45-beta)/1)+"");
+                    } else {
+                        $("#id1").css( "font-variation-settings", "'HROT'"+(gamma/1.5)+",'VROT'"+((beta-45)/-1)+"");
+                        $("#id2").css( "font-variation-settings", "'HROT'"+(gamma/1.5)+",'VROT' 0");
+                        $("#id11").css( "font-variation-settings", "'HROT' 0,'VROT'"+((beta-45)/-1)+"");
+                    }
+                    accessGrant = true;
+                });
+            }
+        })
+            .catch(console.error);
+    } else {
+        window.addEventListener('deviceorientation', (e) => {
+            const beta = e.beta;
+            const gamma = e.gamma;
+            if (beta < 45) {
+                $("#id1").css( "font-variation-settings", "'HROT'"+(gamma/1.5)+",'VROT'"+((45-beta)/1)+"");
+                $("#id2").css( "font-variation-settings", "'HROT'"+(gamma/1.5)+",'VROT' 0");
+                $("#id11").css( "font-variation-settings", "'HROT' 0,'VROT'"+((45-beta)/1)+"");
+            } else {
+                $("#id1").css( "font-variation-settings", "'HROT'"+(gamma/1.5)+",'VROT'"+((beta-45)/-1)+"");
+                $("#id2").css( "font-variation-settings", "'HROT'"+(gamma/1.5)+",'VROT' 0");
+                $("#id11").css( "font-variation-settings", "'HROT' 0,'VROT'"+((beta-45)/-1)+"");
+            }
+        });
+    }
+}
+
+
+function  startup(){
+
+    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+        DeviceOrientationEvent.requestPermission()
+            .then(permissionState => {
+            if (permissionState === 'granted') {
+                $(".overlay").css( "display", "none");
+                window.addEventListener('deviceorientation', (e) => {
+                    const beta = e.beta;
+                    const gamma = e.gamma;
+                    if (beta < 45) {
+                        $("#id1").css( "font-variation-settings", "'HROT'"+(gamma/1.5)+",'VROT'"+((45-beta)/1)+"");
+                        $("#id2").css( "font-variation-settings", "'HROT'"+(gamma/1.5)+",'VROT' 0");
+                        $("#id11").css( "font-variation-settings", "'HROT' 0,'VROT'"+((45-beta)/1)+"");
+                    } else {
+                        $("#id1").css( "font-variation-settings", "'HROT'"+(gamma/1.5)+",'VROT'"+((beta-45)/-1)+"");
+                        $("#id2").css( "font-variation-settings", "'HROT'"+(gamma/1.5)+",'VROT' 0");
+                        $("#id11").css( "font-variation-settings", "'HROT' 0,'VROT'"+((beta-45)/-1)+"");
+                    }
+                    accessGrant = true;
+                });
+            }
+        })
+            .catch(console.error);
+    } else {
+        $(".overlay").css( "display", "none");
+        window.addEventListener('deviceorientation', (e) => {
+            const beta = e.beta;
+            const gamma = e.gamma;
+            if (beta < 45) {
+                $("#id1").css( "font-variation-settings", "'HROT'"+(gamma/1.5)+",'VROT'"+((45-beta)/1)+"");
+                $("#id2").css( "font-variation-settings", "'HROT'"+(gamma/1.5)+",'VROT' 0");
+                $("#id11").css( "font-variation-settings", "'HROT' 0,'VROT'"+((45-beta)/1)+"");
+            } else {
+                $("#id1").css( "font-variation-settings", "'HROT'"+(gamma/1.5)+",'VROT'"+((beta-45)/-1)+"");
+                $("#id2").css( "font-variation-settings", "'HROT'"+(gamma/1.5)+",'VROT' 0");
+                $("#id11").css( "font-variation-settings", "'HROT' 0,'VROT'"+((beta-45)/-1)+"");
+            }
+        });
+    }
+
+}
