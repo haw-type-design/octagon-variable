ab5ff83706a1c6905a316257888df7090ec4244c
simonthi <github@simonthiefes.de>
2023-01-24 13:07:43 +0100

    Fixed error with anchors in M

--- template/main/fontmake.sh
+++ template/main/fontmake.sh
@@ -4,10 +4,10 @@
 
 fontmake 221026_octagon.glyphs -i -o ttf --output-dir ../../export/static/ttf
 fontmake 221026_octagon.glyphs -i -o otf --output-dir ../../export/static/otf
-cp -r instance_ufo/OctagonVariable-HROT-30VROT-30.ufo master_ufo
-cp -r instance_ufo/OctagonVariable-HROT-30VROT30.ufo master_ufo
-cp -r instance_ufo/OctagonVariable-HROT30VROT-30.ufo master_ufo
-cp -r instance_ufo/OctagonVariable-HROT30VROT30.ufo master_ufo
+cp -rf instance_ufo/OctagonVariable-HROT-30VROT-30.ufo master_ufo
+cp -rf instance_ufo/OctagonVariable-HROT-30VROT30.ufo master_ufo
+cp -rf instance_ufo/OctagonVariable-HROT30VROT-30.ufo master_ufo
+cp -rf instance_ufo/OctagonVariable-HROT30VROT30.ufo master_ufo
 fontmake master_ufo/octagon-variable.designspace -o variable --output-dir ../../export/variable/ttf
 mv ../../export/variable/ttf/octagon-variable-VF.ttf ../../export/variable/ttf/OctagonVariableVF.ttf 
 woff2_compress ../../export/variable/ttf/OctagonVariableVF.ttf
